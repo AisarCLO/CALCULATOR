9077919ceb2ce93dc988322aa1dc4899316e504c
Nischal Chaulagai <135114637+AisarCLO@users.noreply.github.com>
2023-09-18 15:47:26 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 let string = "";
+let lastInput = "";
 
 let buttons = document.querySelectorAll('.button');
 
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click',(e)=>{
       if(e.target.innerHTML == '='){
-        string = eval(string);
+        try {
+          string = eval(string);
+        } catch (error) {
+          string = 'ERROR!';
+        }
         document.querySelector('input').value=string;
       } else if(e.target.innerHTML == 'AC'){
         string = "";
         document.querySelector('input').value=string;
       } else if(e.target.innerHTML == 'DEL'){
-        string = eval(string.toString().slice(0,-1));
+        string = string.slice(0,-1);
         document.querySelector('input').value=string;
     }
       else{
-       string = string + e.target.innerHTML;
+       if (['+', '-', '*', '/'].includes(e.target.innerHTML) && ['+', '-', '*', '/'].includes(lastInput)) {
+         string = 'ERROR!';
+       } else {
+         string = string + e.target.innerHTML;
+       }
        document.querySelector('input').value = string;
+       lastInput = e.target.innerHTML;
     }})
 })
+
+if (string === '') {
+  document.querySelector('input').value = '0';
+}
